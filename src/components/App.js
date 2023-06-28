e53cefddfaf40dda097da2ebbec8732ce2e6d43e
Akshay Chaudhary <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-06-28 05:55:48 +0000

    third

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,9 @@ import './../styles/App.css';
 
 const App = () => {
   const tabs = [
-    { title: 'Tab 1', content: 'This is a Content for Tab 1' },
-    { title: 'Tab 2', content: 'This is a Content for Tab 2' },
-    { title: 'Tab 3', content: 'This is a Content for Tab 3' },
+    { title: 'Tab 1', content: 'This is the content for Tab 1.' },
+    { title: 'Tab 2', content: 'This is the content for Tab 2.' },
+    { title: 'Tab 3', content: 'This is the content for Tab 3.' },
   ];
 
   return (
