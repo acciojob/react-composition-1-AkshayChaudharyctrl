ea2eeffcd0361dd492f9d85d4ac47461f2c7f03d
Akshay Chaudhary <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-06-28 05:31:39 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 
 import React from "react";
+import Tabs from "./../Tabs";
 import './../styles/App.css';
 
 const App = () => {
+  const tabs = [
+    { title: 'Tab 1', content: 'Content for Tab 1' },
+    { title: 'Tab 2', content: 'Content for Tab 2' },
+    { title: 'Tab 3', content: 'Content for Tab 3' },
+  ];
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Tabs Example</h1>
+      <Tabs tabs={tabs} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
